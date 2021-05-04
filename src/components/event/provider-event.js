64c76fb71419bb65f0757dbecf4c85160db7b691
hdcaruth <hdcaruthers@gmail.com>
2021-05-03 20:39:36 -0700

    Adding bunch of crust

--- src/components/event/provider-event.js
+++ src/components/event/provider-event.js
@@ -1,6 +1,13 @@
 import AgoraRTC from "agora-rtc-sdk-ng"
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { createElement } from "react-native-web";
+
+
+// Trying with vanilla React Native Agora Client
+import {Platform, ScrollView, Text, TouchableOpacity, PermissionsAndroid} from 'react-native'
+// Import the RtcEngine class and view rendering components into your project.
+import RtcEngine, {RtcLocalView, RtcRemoteView, VideoRenderMode} from 'react-native-agora'
 
 const rtc = {
   client: null,
@@ -18,6 +25,40 @@ const rtc_options = {
 export default class ProviderEvent extends React.Component {
 
   renderControlTray() {
+
+  }
+
+  renderProviderVideo() {
+    // Style this. Upper lefthand corner? 
+    return <div width={640} height={500}></div>;
+  }
+
+  init = async () => {
+    this._engine = await RtcEngine.create(appId)
+    // Enable the video module.
+    await this._engine.enableVideo()
+
+    // Listen for the JoinChannelSuccess callback.
+    // This callback occurs when the local user successfully joins the channel.
+    this._engine.addListener('JoinChannelSuccess', (channel, uid, elapsed) => {
+        console.log('JoinChannelSuccess', channel, uid, elapsed)
+    })
+  }
+
+  _renderVideos = () => {
+    const {joinSucceed} = this.state
+    return (
+        <View>
+            <RtcLocalView.SurfaceView
+                style={styles.max}
+                channelId={this.state.channelName}
+                renderMode={VideoRenderMode.Hidden}/>
+        </View>
+    );
+}
+
+  componentDidMount() {
+    this.init();
   }
 
   async componentDidMount() {
@@ -25,12 +66,40 @@ export default class ProviderEvent extends React.Component {
     await rtc.client.join(rtc_options.appId, rtc_options.channel, rtc_options.token);
     rtc.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
     rtc.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+    console.log(rtc.localVideoTrack);
+
+    // const rig = React.createElement("div", {style: {width: "640", height: "480"}}, null);
+    // rig.style.width = "640px";
+    // rig.style.height = "480px";
+
+    const rig = createElement("div", {id: 'rig', style: {width: "640", height: "480"}}, null);
+    rtc.localVideoTrack.play(rig);
+    this.setState({isVideoContainerCreated: true});
+    
     await rtc.client.publish([rtc.localAudioTrack, rtc.localVideoTrack]);
+		
+    rtc.client.on("user-published", async (user, mediaType) => {
+
+      console.log(user); 
+      console.log(mediaType);
+			// await rtc.client.subscribe(user, mediaType);
+			// if (mediaType === "audio") {
+			// 	const remoteAudioTrack = user.audioTrack;
+			// 	remoteAudioTrack.play();
+      //           if (this.isProvider(user.uid)) {
+      //               this.setState({provider: this.getName(user.uid)});
+      //           } else {
+      //               this.setState(prevState => ({presentUsers: prevState.presentUsers.concat(user.uid)}));
+      //           }
+			// }
+		});
+
   }
 
   render() {
-    return (
-      null
-    );
+    console.error('here');
+    return null;
+    // return React.createElement("div", {id: 'rig', style: {width: "640", height: "480"}}, null);
+    // return renderProviderVideo(); 
   }
 }
